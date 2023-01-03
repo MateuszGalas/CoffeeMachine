0c98171549bab8be083aac25a75549ecb87f774f
Mateusz Galaś <mateuszgalas94@gmail.com>
2023-01-03 19:11:18 +0100

    Refactoring code into different form.

--- Main.kt
+++ Main.kt
@@ -1,9 +1,13 @@
 package machine
 
-enum class Ingredients (val water: Int, val milk: Int, val coffee: Int, val price: Int) {
+enum class Ingredients(val water: Int, val milk: Int, val coffee: Int, val price: Int) {
     ESPRESSO(250, 0, 16, 4),
-    LATTE(350,75, 20, 7),
+    LATTE(350, 75, 20, 7),
     CAPPUCCINO(200, 100, 12, 6)
+}
+
+enum class STATE {
+    MENU, BUY, FILL_WATER, FILL_MILK, FILL_COFFEE, FILL_CUPS
 }
 
 class CoffeeMachine() {
@@ -12,6 +16,7 @@ class CoffeeMachine() {
     private var coffeeInMachine: Int = 120
     private var disposableCups = 9
     private var money = 550
+    private var state = STATE.MENU
 
     private val espresso = Ingredients.ESPRESSO
     private val latte = Ingredients.LATTE
@@ -26,13 +31,12 @@ class CoffeeMachine() {
         println("$$money of money\n")
     }
 
-    private fun buy() {
-        println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ")
-        when (readln()) {
+    private fun buy(action: String) {
+        when (action) {
             "1" -> changeAmount(espresso)
             "2" -> changeAmount(latte)
             "3" -> changeAmount(cappuccino)
-            "back" -> { }
+            "back" -> {}
         }
         println()
     }
@@ -51,32 +55,65 @@ class CoffeeMachine() {
         money += coffee.price
     }
 
-    private fun fill() {
-        println("Write how many ml of water you want to add:")
-        waterInMachine += readln().toInt()
-        println("Write how many ml of milk you want to add:")
-        milkInMachine += readln().toInt()
-        println("Write how many grams of coffee beans you want to add:")
-        coffeeInMachine += readln().toInt()
-        println("Write how many disposable cups you want to add:")
-        disposableCups += readln().toInt()
-        println()
-    }
-
     private fun take() {
         println("I gave you $money\n")
         money = 0
     }
 
+    private fun getCommand() {
+        when (state) {
+            STATE.MENU -> println("Write action (buy, fill, take, remaining, exit):")
+            STATE.FILL_MILK -> println("Write how many ml of milk you want to add:")
+            STATE.FILL_COFFEE -> println("Write how many grams of coffee beans you want to add:")
+            STATE.FILL_CUPS -> println("Write how many disposable cups you want to add:")
+            STATE.FILL_WATER -> println("Write how many ml of water you want to add:")
+            STATE.BUY -> println(
+                "What do you want to buy? 1 - espresso, 2 - latte, " +
+                        "3 - cappuccino, back - to main menu:"
+            )
+        }
+    }
+
     fun run() {
         while (true) {
-            println("Write action (buy, fill, take, remaining, exit):")
-            when (readln()) {
-                "buy" -> buy()
-                "fill" -> fill()
-                "take" -> take()
-                "remaining" -> status()
-                "exit" -> break
+            getCommand()
+            val action = readln()
+            when (state) {
+                STATE.MENU -> {
+                    when (action) {
+                        "buy" -> state = STATE.BUY
+                        "fill" -> state = STATE.FILL_WATER
+                        "take" -> take()
+                        "remaining" -> status()
+                        "exit" -> break
+                    }
+                }
+
+                STATE.BUY -> {
+                    buy(action)
+                    state = STATE.MENU
+                }
+
+                STATE.FILL_WATER -> {
+                    waterInMachine += action.toInt()
+                    state = STATE.FILL_MILK
+                }
+
+                STATE.FILL_MILK -> {
+                    milkInMachine += action.toInt()
+                    state = STATE.FILL_COFFEE
+                }
+
+                STATE.FILL_COFFEE -> {
+                    coffeeInMachine += action.toInt()
+                    state = STATE.FILL_CUPS
+                }
+
+                STATE.FILL_CUPS -> {
+                    disposableCups += action.toInt()
+                    println()
+                    state = STATE.MENU
+                }
             }
         }
     }
